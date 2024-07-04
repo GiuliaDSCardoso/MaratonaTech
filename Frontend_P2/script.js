ee08a8a9b44a7e7d3fc9a9dfe801ee14f8f23e14
Giulia Cardoso <imgirassoia@gmail.com>
2024-07-04 15:59:32 -0300

    Update script.js

--- Frontend_P2/script.js
+++ Frontend_P2/script.js
@@ -2,20 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contact-form');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Previne o envio padrão do formulário
+        event.preventDefault();
 
-        // Captura dos valores dos campos
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const phone = document.getElementById('phone').value;
         const message = document.getElementById('message').value;
 
-        // Aqui você pode realizar validações dos campos, se necessário
+    
 
-        // Exibe a mensagem de sucesso
+      
         alert('Mensagem enviada!');
 
-        // Limpa os campos do formulário após enviar
+        
         form.reset();
     });
 });
